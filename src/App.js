954683030c8436f067d8b0340f19c5e1f2f630b7
danweill <danweill0@gmail.com>
2024-03-10 09:41:54 +0200

    add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from './MapComponent';
+import {useState} from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MapComponent.jsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from 'react';
+import { loadModules } from 'esri-loader';
+
+const MapComponent = () => {
+    useEffect(() => {
+      loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer'], { css: true })
+        .then(([Map, MapView, FeatureLayer]) => {
+          const map = new Map({
+            basemap: 'topo-vector'
+          });
+  
+          const view = new MapView({
+            container: 'viewDiv',
+            map: map,
+            
+            center: [35, 31], 
+            zoom: 7
+          });
+  
+          const citiesLayer = new FeatureLayer({
+            url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Cities/FeatureServer/0',
+            outFields: ['CITY_NAME', 'POPULATION'],
+            popupTemplate: {
+              title: '{CITY_NAME}',
+              content: 'Population: {POP}',
+              
+            },
+            renderer: {
+              type: 'simple',
+              symbol: {
+                type: 'simple-marker',
+                style: 'circle',
+                color: [255, 0, 0], // Red color
+                size: 6
+              }
+            }
+          });
+          console.log(citiesLayer);
+          map.add(citiesLayer);
+        })
+        .catch((err) => console.error(err));
+    }, []);
+  
+    return <div id="viewDiv" style={{ height: '100vh', width: '100%' }} />;
+  };
+  
+  export default MapComponent;
